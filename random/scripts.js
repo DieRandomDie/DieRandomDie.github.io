3682ccd608088f122793a36f4e0a1a0b05cfa0e1
DieRandomDie <EricRandom1023@gmail.com>
2024-05-28 14:56:50 -0500

    added last guess and functionality to result

--- random/scripts.js
+++ random/scripts.js
@@ -3,7 +3,9 @@ maximum = $("#max")
 range_confirmation = $("#range-confirmation")
 guess = $("#guess")
 result_confirmation = $("#result-confirmation")
+last_guess = $("#last-guess")
 let answer = 0
+let tries = 0
 
 $("#range-submit").on("click", function () {
     newRange(minimum.val(),maximum.val())
@@ -28,15 +30,24 @@ function newRange(x, y) {
     let min = parseInt(x)
     let max = parseInt(y)
     answer = Math.floor(Math.random() * (max - min + 1)) + min
+    tries = 0
     result_confirmation.text(`Try guessing a number between ${min} and ${max}!`)
 }
 
 function processGuess(x) {
     guess.val('')
+    tries++
+    last_guess.text(`Last guess: ${x}`)
     if (x!==answer) {
-        result_confirmation.text(`You guessed ${x}, try a ${answer>x ? "higher": "lower"} number.`)
+        result_confirmation.text(`Try a ${answer>x ? "higher": "lower"} number.`)
     } else {
         result_confirmation.text("Correct!")
+        $("#history").append(`<p>Game: ${minimum.val()}-${maximum.val()}, guessed ${answer} in ${tries} tries!</p>`)
+        tries = 0
+        setTimeout(function () {
+            newRange(minimum.val(),maximum.val())
+            last_guess.text("Last guess:")
+        },3000)
     }
 }
 
